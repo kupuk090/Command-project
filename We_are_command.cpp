c6c19a15ed247b98b913745020fc525f3906bf8e
kupuk090 <kupuk090@gmail.com>
2016-03-28 20:30:27 +0300

    Added files via upload

--- We_are_command.cpp
+++ We_are_command.cpp
@@ -1,51 +1,49 @@
 #include "We_are_command.h"
-#include <iostream>
 
-using namespace std;
-
-	//РІРѕР·РІСЂР°С‰Р°РµРј СЂР°РґРёСѓСЃ РѕРїРёСЃР°РЅРЅРѕР№ РѕРєСЂСѓР¶РЅРѕСЃС‚Рё
-    	double n_angle:: get_radius()
+	//возвращаем радиус описанной окружности
+	
+    double n_angle:: get_radius()
 	{
 		return radius;
 	}
 	
-	//РІРѕР·РІСЂР°С‰Р°РµРј x РєРѕРѕСЂРґРёРЅР°С‚Сѓ С†РµРЅС‚СЂР°
+	//возвращаем x координату центра
 	double n_angle:: get_x()
 	{
 		return x;
 	}
 	
-	//РІРѕР·РІСЂР°С‰Р°РµРј y РєРѕРѕСЂРґРёРЅР°С‚Сѓ С†РµРЅС‚СЂР°
+	//возвращаем y координату центра
 	double n_angle:: get_y()
 	{
 		return y;
 	}
 	
-	//РІРѕР·РІСЂР°С‰Р°РµРј n
+	//возвращаем n
 	int n_angle:: get_n()
 	{
 		return n;
 	}
 
-	//РїРѕР»СѓС‡Р°РµРј РІРµР»РёС‡РёРЅСѓ СЃС‚РѕСЂРѕРЅС‹ n-СѓРіРѕР»СЊРЅРёРєР°
+	//получаем величину стороны n-угольника
 	double n_angle :: get_side()
 	{
 		return 2 * radius * sin(M_PI / n);
 	}
 
-	//РїРѕР»СѓС‡Р°РµРј РїР»РѕС‰Р°РґСЊ n-СѓРіРѕР»СЊРЅРёРєР°
+	//получаем площадь n-угольника
 	double n_angle::get_area()
 	{
 		return (n / 2)*pow(radius, 2)*sin(2 * M_PI / n);
 	}
 	
-	//РїРѕР»СѓС‡Р°РµРј РїРµСЂРёРјРµС‚СЂ n-СѓРіРѕР»СЊРЅРёРєР°
+	//получаем периметр n-угольника
 	double n_angle::get_perimeter()
 	{
 		return get_side()*n;
 	}
 	
-	//РїРѕР»СѓС‡Р°РµРј РїР°СЂР°РјРµС‚СЂС‹ n-СѓРіРѕР»СЊРЅРёРєР°
+	//получаем параметры n-угольника
 	void n_angle :: return_parameters()
 	{
 		cout << "parametrs of n-angle:\n";
@@ -61,14 +59,14 @@ using namespace std;
 			 << get_n();
 	}
 	
-	//РїРµСЂРµРјРµС‰РµРЅРёРµ С†РµРЅСЂР° РІ РЅРѕРІС‹Рµ РєРѕРѕСЂРґРёРЅР°С‚С‹
+	//перемещение ценра в новые координаты
 	void n_angle:: move(double x_, double y_)
 	{
 		x = x_;
 		y = y_;
 	}
 	
-	//РёР·РјРµРЅРµРЅРёРµ СЂР°Р·РјРµСЂРѕРІ РЅР° РєР°РєСѓСЋ-Р»РёР±Рѕ РІРµР»РёС‡РёРЅСѓ 
+	//изменение размеров на какую-либо величину 
 	void n_angle:: resizing_by_side(double a)
 	{
 		radius = (get_side() + a) / 2 / sin(M_PI / n);
@@ -79,10 +77,10 @@ using namespace std;
 		radius = radius + a;
 	}
 	
-	//РїРµСЂРµРіСЂСѓР·РєР° РѕРїРµСЂР°С‚РѕСЂР° СЃСЂР°РІРЅРµРЅРёСЏ РґРІСѓС… n-СѓРіРѕР»СЊРЅРёРєРѕРІ
+	//перегрузка оператора сравнения двух n-угольников
 	bool n_angle::operator== (n_angle n)
 	{
-		if ((this->radius == n.radius) && (this->n == n.n))
+		if ((abs(this->radius - n.radius) < 0.000000001) && (this->n == n.n))
 		{
 			return true;
 		}
@@ -92,7 +90,7 @@ using namespace std;
 		}
 	}
 	
-	//РѕРїСЂРµРґРµР»РµРЅРёРµ РїРµСЂРµСЃРµС‡РµРЅРёСЏ РґРІСѓС… n-СѓРіРѕР»СЊРЅРёРєРѕРІ
+	//определение пересечения двух n-угольников
 	bool n_angle :: crossing(n_angle second)
 	{
 		double l;
@@ -108,7 +106,7 @@ using namespace std;
 
 	}
 	
-	//РѕРїСЂРµРґРµР»РµРЅРёСЏ РІС…РѕР¶РґРµРЅРёСЏ n-СѓРіРѕР»СЊРЅРёРєР° second РІ n-СѓРіРѕР»СЊРЅРёРє first
+	//определения вхождения n-угольника second в n-угольник first
 	bool n_angle :: accessory(n_angle second)
 	{
 		double l;
@@ -132,7 +130,7 @@ using namespace std;
 		
 	}
 	
-	//РѕРїСЂРµРґРµР»СЏРµРј РєРІР°РґСЂР°С‚ Р»Рё
+	//определяем квадрат ли
 	bool n_angle:: is_it_square()
 	{
 		if (n == 4) 
@@ -153,92 +151,104 @@ using namespace std;
 		y = y_;
 	}
 	
-	
-	
-	
-	
-	
-	//РѕРїСЂРµРґРµР»РµРЅРёРµ РїРµСЂРµСЃРµС‡РµРЅРёСЏ СЌР»Р»РёРїСЃР° Рё n-СѓРіРѕР»СЊРЅРёРєР°
-	bool crossing(n_angle n, ellipse el)
+
+	//функция знака
+	int Sign(double x)
 	{
-		return 0;
-	}
-	
-	bool crossing(ellipse el, n_angle n)
-	{
-		if (crossing(n, el)) return true;
-		else return false;
-	}
-	
-	//РѕРїСЂРµРґРµР»РµРЅРёРµ РІС…РѕР¶РґРµРЅРёСЏ СЌР»Р»РёРїСЃР° РІ n-СѓРіРѕР»СЊРЅРёРє
-	bool accessory(n_angle n, ellipse el)
-	{
-		return 0;
-	}
-	
-	bool accessory(ellipse el, n_angle n)
-	{
-		return 0;
+		return x < 0 ? -1 : ((x == 0) ? 0 : 1);
 	}
 
+	//определение принадлежности точек 
+	bool ellipse::beloning_el(ellipse second, double x, double y)
+	{
+		double a = this->get_major_axis();
+		double b = this->get_minor_axis();
+		double c = second.major_axis;
+		double d = second.minor_axis;
+		double x01 = this->get_x();
+		double y01 = this->get_y();
+		double x02 = second.get_x();
+		double y02 = second.get_y();
+		double f1 = pow(x - x01, 2) / (a*a) + pow(y - y01, 2) / (b*b) - 1;
+		double f2 = pow(x - x02, 2) / (c*c) + pow(y - y02, 2) / (d*d) - 1;
 
+		if ((f1 <= 0) && (f2 <= 0))
+			return true;
+		else
+			return false;
+	}
+	bool ellipse::beloning_n(n_angle second, double x, double y)
+	{
+		double a = this->get_major_axis();
+		double b = this->get_minor_axis();
+		double c = second.get_radius();
+		double d = second.get_radius();
+		double x01 = this->get_x();
+		double y01 = this->get_y();
+		double x02 = second.get_x();
+		double y02 = second.get_y();
+		double f1 = pow(x - x01, 2) / (a*a) + pow(y - y01, 2) / (b*b) - 1;
+		double f2 = pow(x - x02, 2) / (c*c) + pow(y - y02, 2) / (d*d) - 1;
 
-
+		if ((f1 <= 0) && (f2 <= 0))
+			return true;
+		else
+			return false;
+	}
 
 	
-	
-	//РІРѕР·РІСЂР°С‰Р°РµРј major_axis
-	double ellipse:: get_major_axis()
+	//возвращаем major_axis
+	double ellipse::get_major_axis()
 	{
 		return major_axis;
 	}
 	
-	//РІРѕР·РІСЂР°С‰Р°РµРј minor_axis
-	double ellipse:: get_minor_axis()
+	//возвращаем minor_axis
+	double ellipse::get_minor_axis()
 	{
 		return minor_axis;
 	}
 	
-	//РІРѕР·РІСЂР°С‰Р°РµРј x РєРѕРѕСЂРґРёРЅР°С‚Сѓ С†РµРЅС‚СЂР°
-	double ellipse:: get_x()
+	//возвращаем x координату центра
+	double ellipse::get_x()
 	{
 		return x;
 	}
 	
-	//РІРѕР·РІСЂР°С‰Р°РµРј y РєРѕРѕСЂРґРёРЅР°С‚Сѓ С†РµРЅС‚СЂР°
-	double ellipse:: get_y()
+	//возвращаем y координату центра
+	double ellipse::get_y()
 	{
 		return y;
 	}
 
-	 //РїРѕР»СѓС‡Р°РµРј РїР»РѕС‰Р°РґСЊ СЌР»Р»РёРїСЃР°
+	 //получаем площадь эллипса
 	 double ellipse::get_area()
 	 {
 		 return major_axis*minor_axis*M_PI;	 
 	 }
 	 
-	 //РїРѕР»СѓС‡Р°РµРј РїРµСЂРёРјРµС‚СЂ СЌР»Р»РёРїСЃР°
+	 //получаем периметр эллипса
 	 double ellipse::get_perimeter()
 	 {
 		 return (4 * (major_axis*minor_axis*M_PI + pow((major_axis - minor_axis), 2)) / (major_axis + minor_axis));
 	 
 	 }
 	 
-	 //РїРµСЂРµРјРµС‰РµРЅРёРµ С†РµРЅСЂР° РІ РЅРѕРІС‹Рµ РєРѕРѕСЂРґРёРЅР°С‚С‹
+	 //перемещение ценра в новые координаты
 	 void ellipse::move(double x_, double y_)
 	 {
 		 x = x_;
 		 y = y_;
 	 }
 
-	 //РёР·РјРµРЅРµРЅРёРµ СЂР°Р·РјРµСЂРѕРІ РЅР° РєР°РєСѓСЋ-Р»РёР±Рѕ РІРµР»РёС‡РёРЅСѓ 
-	 void ellipse:: resizing(double x, double y)
+	 //изменение размеров на какую-либо величину 
+	 void ellipse::resizing(double x, double y)
 	 {
 		 major_axis += x;
 		 minor_axis += y;
 	 }
 
-	 //РїРµСЂРµРіСЂСѓР·РєР° РѕРїРµСЂР°С‚РѕСЂР° СЃСЂР°РІРЅРµРЅРёСЏ РґРІСѓС… СЌР»Р»РёРїСЃРѕРІ
+	 //перегрузка оператора сравнения двух эллипсов
 	 bool ellipse::operator== (ellipse second)
 	 {
 		 if (this->get_major_axis() != second.get_major_axis())
@@ -258,69 +268,65 @@ using namespace std;
 	}
 
 
-
-	//РїРѕР»СѓС‡Р°РµРј СЂР°РґРёСѓСЃ СЌР»Р»РёРїСЃР° Рє С‚РѕС‡РєРµ Р»РµР¶Р°С‰РµР№ РЅР° РїСЂСЏРјРѕР№, СЃРѕРµРґРёРЅСЏСЋС‰РµР№ С†РµРЅС‚СЂС‹ СЂР°СЃСЃРјР°С‚СЂРёРІР°РµРјРѕРіРѕ СЌР»Р»РёРїСЃР° Рё РґСЂСѓРіРѕР№ С„РёРіСѓСЂС‹
-	 double ellipse::get_radius(double a, double b, double x1, double y1, double x2, double y2)
-	 {
-		 double dx = x2 - x1;
-		 double dy = y2 - y1;
-
-		 if (dx == 0)
-			 return b;
-		 else
-		 {
-			 return a*b / sqrt((b*b*dx*dx / (dx*dx + dy*dy)) + (a*a*dy*dy / (dx*dx + dy*dy)));
-		 }
-	 }
-
-	 //РѕРїСЂРµРґРµР»РµРЅРёРµ РїРµСЂРµСЃРµС‡РµРЅРёСЏ РґРІСѓС… СЌР»Р»РёРїСЃРѕРІ
+	 //определение пересечения двух эллипсов
 	 bool ellipse::crossing(ellipse second)
 	 {
-		 double l;
-		 l = sqrt(pow((this->get_x() - second.get_x()), 2) + pow((this->get_y() - second.get_y()), 2));
-		 if ((get_radius(this->get_major_axis(), this->get_minor_axis(), this->get_x(), this->get_y(), second.get_x(), second.get_y()) + get_radius(second.get_major_axis(), second.get_minor_axis(), second.get_x(), second.get_y(), this->get_x(), this->get_y())) >= l)
 		 {
-			 return true;
-		 }
-		 else
-		 {
+			 double a = this->get_major_axis();
+			 double b = this->get_minor_axis();
+			 double c = second.major_axis;
+			 double d = second.minor_axis;
+			 double x0 = this->get_x() - second.get_x();
+			 double y0 = this->get_y() - second.get_y();
+			 double t = 0;
+			 double f = pow((a*cos(t) + x0), 2) / (c*c) + pow(b*sin(t) + y0, 2) / (d*d) - 1;
+			 double tmp = 0;
+
+			 while (t < 2 * M_PI)
+			 {
+				 tmp = f;
+				 t = t + 0.001;
+				 f = pow((a*cos(t) + x0), 2) / (c*c) + pow((b*sin(t) + y0), 2) / (d*d) - 1;
+				 if (Sign(tmp) != Sign(f))
+					 return true;
+			 }
 			 return false;
 		 }
 	 }
 
-	 //РѕРїСЂРµРґРµР»РµРЅРёСЏ РІС…РѕР¶РґРµРЅРёРµ СЌР»Р»РёРїСЃР° second РІ РЅР°С€ СЌР»Р»РёРїСЃ
+	 //определения вхождение эллипса second в наш эллипс
 	 bool  ellipse::accessory(ellipse second)
 	 {
-		 double l;
-		 l = sqrt(pow((this->get_x() - second.get_x()), 2) + pow((this->get_y() - second.get_y()), 2));
-		 if (this->crossing(second))
-		 {
-			 if (abs(get_radius(this->get_major_axis(), this->get_minor_axis(), this->get_x(), this->get_y(), second.get_x(), second.get_y()) - get_radius(second.get_major_axis(), second.get_minor_axis(), second.get_x(), second.get_y(), this->get_x(), this->get_y())) >= l)
-			 {
-			 
-				 return true;
-			 }
-			 else
-			 {
-				 return false;
-			 }
-		 }
+		 double a = this->get_major_axis();
+		 double b = this->get_minor_axis();
+		 double c = second.major_axis;
+		 double d = second.minor_axis;
+		 double x01 = this->get_x();
+		 double y01 = this->get_y();
+		 double x02 = second.get_x();
+		 double y02 = second.get_y();
+
+		 if ((this->beloning_el(second, x01, y01 + b)) && (this->beloning_el(second, x01, y01 - b)) && (this->beloning_el(second, x01 + a, y01)) && (this->beloning_el(second, x01 - a, y01)))
+			 return true;
+
+		 if ((this->beloning_el(second, x02, y02 + d)) && (this->beloning_el(second, x02, y02 - d)) && (this->beloning_el(second, x02 + c, y02)) && (this->beloning_el(second, x02 - c, y02)))
+			 return true;
+
 		 else
-		 {
 			 return false;
-		 }
 	 }
 
-	 //РѕРїСЂРµРґРµР»СЏРµРј РєСЂСѓРі Р»Рё
+	 //определяем круг ли
 	 bool ellipse::is_it_circle()
 	 {
-		 if (abs(major_axis - minor_axis) < 0.000000001)			 return true;
+		 if (abs(major_axis - minor_axis) < 0.000000001)
+			 return true;
 		 else
 			 return false;
 	 }
 
-	 //РІС‹РІРѕРґ РїР°СЂР°РјРµС‚СЂРѕРІ
-	 void ellipse :: return_parameters()
+	 //вывод параметров
+	 void ellipse::return_parameters()
 	 {
 		 cout << "parametrs of n-angle:\n";
 		 cout << "ellipse of major_axis:\n"
@@ -334,9 +340,7 @@ using namespace std;
 		
 	 }
 	 
-	 
-
-	 //РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РєР»Р°СЃСЃР°
+	 //конструктор класса
 	 ellipse::ellipse(double a, double b, double x_, double y_)
 	 {
 		 major_axis = a;
@@ -345,7 +349,7 @@ using namespace std;
 		 y = y_;
 	 }
 	 
-	 //СЃСЂР°РІРЅРёРІР°РµРј РїРѕ РїР»РѕС‰Р°РґРё РЅР°С€ n-СѓРіРѕР»СЊРЅРёРє Рё СЌР»Р»РёРїСЃ
+	 //сравниваем по площади наш n-угольник и эллипс
 	 bool operator== (ellipse el, n_angle n)
 	 {
 		 if (abs(n.get_area() - el.get_area()) < 0.000000001)
@@ -358,4 +362,67 @@ using namespace std;
 	 {
 		 if (n == el) return true;
 		 else return false;
+	 }
+
+	 //определение пересечения эллипса и n-угольника
+	 bool crossing(n_angle n, ellipse el)
+	 {
+		 double a = el.get_major_axis();
+		 double b = el.get_minor_axis();
+		 double c = n.get_radius();
+		 double d = n.get_radius();
+		 double x0 = el.get_x() - n.get_x();
+		 double y0 = el.get_y() - n.get_y();
+		 double t = 0;
+		 double f = pow((a*cos(t) + x0), 2) / (c*c) + pow(b*sin(t) + y0, 2) / (d*d) - 1;
+		 double tmp = 0;
+
+		 while (t < 2 * M_PI)
+		 {
+			 tmp = f;
+			 t = t + 0.001;
+			 f = pow((a*cos(t) + x0), 2) / (c*c) + pow((b*sin(t) + y0), 2) / (d*d) - 1;
+			 if (Sign(tmp) != Sign(f))
+				 return true;
+		 }
+		 return false;
+	 }
+	 bool crossing(ellipse el, n_angle n)
+	 {
+		 if (crossing(n, el))
+			 return true;
+		 else
+			 return false;
+	 }
+
+
+	 //определение вхождения эллипса в n-угольник
+	 bool accessory(n_angle n, ellipse el)
+	 {
+		 if (accessory(el, n))
+			 return true;
+		 else
+			 return false;
+	 }
+	 
+	 //определение вхождения n-угольника в эллипс
+	 bool accessory(ellipse el, n_angle n)
+	 {
+		 double a = el.get_major_axis();
+		 double b = el.get_minor_axis();
+		 double c = n.get_radius();
+		 double d = n.get_radius();
+		 double x01 = el.get_x();
+		 double y01 = el.get_y();
+		 double x02 = n.get_x();
+		 double y02 = n.get_y();
+
+		 if ((el.beloning_n(n, x01, y01 + b)) && (el.beloning_n(n, x01, y01 - b)) && (el.beloning_n(n, x01 + a, y01)) && (el.beloning_n(n, x01 - a, y01)))
+			 return true;
+
+		 if ((el.beloning_n(n, x02, y02 + d)) && (el.beloning_n(n, x02, y02 - d)) && (el.beloning_n(n, x02 + c, y02)) && (el.beloning_n(n, x02 - c, y02)))
+			 return true;
+
+		 else
+			 return false;
 	 }
