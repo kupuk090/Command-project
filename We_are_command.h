5b1630dc50a0688e09c21d48e56631fc9c96b837
kupuk090 <kupuk090@gmail.com>
2016-03-28 20:30:43 +0300

    Added files via upload

--- We_are_command.h
+++ We_are_command.h
@@ -1,106 +1,94 @@
-#pragma once
 #define _USE_MATH_DEFINES
+
 #include <stdlib.h>
 #include <math.h>
 #include <iostream>
 
-
 using namespace std;
 
-
+#ifndef WE_ARE_COMMAND_H
+#define WE_ARE_COMMAND_H
 
 class n_angle
 {
 private:
-
 	double radius, x = 0, y = 0;
 	int n;
 
 public:
 	//возвращаем радиус описанной окружности
 	double get_radius();
-
 	//возвращаем x координату центра
 	double get_x();
-
 	//возвращаем y координату центра
 	double get_y();
-
 	//возвращаем n
 	int get_n();
 
 	//получаем величину стороны n-угольника
 	double get_side();
-
+	
 	//получаем площадь n-угольника
 	double get_area();
-
 	//получаем периметр n-угольника
 	double get_perimeter();
 
-	//получаем параметры n-угольника
-	void return_parametrs();
 
 	//перемещение ценра в новые координаты
 	void move(double x_, double y_);
-
+	
 	//изменение размеров на какую-либо величину (т.е. +- какое-то число)
 	void resizing_by_side(double x);
-
 	void resizing_by_rad(double x);
-
+	
 	//перегрузка оператора сравнения двух n-угольников
 	bool operator== (n_angle n);
-
+	
 	//определение пересечения двух n-угольников
 	bool crossing(n_angle second);
-
-	//определения вхождения n-угольника second в n-угольник first
+	
+	//определение вхождения n-угольника second в наш n-угольник
 	bool accessory(n_angle second);
 
 	//определяем квадрат ли
 	bool is_it_square();
+	
 
 	//конструктор класса
 	n_angle(double rad, int n_, double x_ = 0, double y_ = 0);
+	
 
+	//вывод параметров
+	void return_parameters();	
 };
 
-
-int Sign(double x);
-
-   class ellipse
+class ellipse
 {
 private:
-		
 	double major_axis, minor_axis, x = 0, y = 0;
-	double angle = 0 ;
 
 public:
 	//возвращаем major_axis
 	double get_major_axis();
-	
 	//возвращаем minor_axis
 	double get_minor_axis();
-	
 	//возвращаем x координату центра
 	double get_x();
-	
 	//возвращаем y координату центра
 	double get_y();
-	
+
 	//получаем площадь эллипса
 	double get_area();
-	
 	//получаем периметр эллипса
 	double get_perimeter();
 	
+
 	//перемещение ценра в новые координаты
 	void move(double x_, double y_);
 
 	//изменение размеров на какую-либо величину (т.е. +- какое-то число)
 	void resizing(double x, double y = 0);
-	
+
 	//перегрузка оператора сравнения двух эллипсов
 	bool operator== (ellipse second);
 
@@ -113,41 +101,31 @@ public:
 	//определяем круг ли
 	bool is_it_circle();
 
+
 	//конструктор класса
 	ellipse(double a, double b, double x_ = 0, double y_ = 0);
 
 	//вывод параметров
 	void return_parameters();
-	
-	//получаем радиус эллипса
-	double get_radius(double a, double b, double x1, double y1, double x2, double y2);
+
+
+
 
 	//сравниваем по площади наш n-угольник и эллипс
 	friend bool operator== (n_angle n, ellipse el);
-
 	friend bool operator== (ellipse el, n_angle n);
-
 	//определение пересечения эллипса и n-угольника
 	friend bool crossing(n_angle n, ellipse el);
-
 	friend bool crossing(ellipse el, n_angle n);
-
 	//определение вхождения эллипса в n-угольник
 	friend bool accessory(n_angle n, ellipse el);
-
 	friend bool accessory(ellipse el, n_angle n);
-	
+
 	//определение принадлежности точек 
-	bool beloning(ellipse second,double x, double y);
+	bool beloning_el(ellipse second, double x, double y);
+	bool beloning_n(n_angle second, double x, double y);
+};
 
-	bool beloning2(n_angle second, double x, double y);
+int Sign(double x);
 
-	bool rotnaoborot(int kirusha);
-	
-   };
-  
-  
-	
-
-
-
+#endif
